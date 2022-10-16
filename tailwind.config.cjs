5a90b290405687ac682f135d2ec70a3027476327
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-10-17 06:26:05 +0700

    feat: finish layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#00AD4D',
+				secondary: '#C0D82C',
+				tertiary: '#004D42'
+			}
+		},
 	},
 	plugins: [],
 }
