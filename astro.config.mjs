ae12b0d8f61c52b6539e77bcd319e2a15775f62c
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-10-17 06:42:16 +0700

    fix: disable SSR for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,6 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
-  output: "server",
-  adapter: vercel()
 });
