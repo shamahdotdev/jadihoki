1e8d35c84429b25a7fed063e1a9b754812ac7615
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-10-20 08:42:14 +0700

    feat: layout revision and add icons and manifest

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#00AD4D',
-				secondary: '#C0D82C',
-				tertiary: '#004D42'
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00AD4D',
+        secondary: '#C0D82C',
+        tertiary: '#004D42',
+      },
+    },
+  },
+  plugins: [],
 }
