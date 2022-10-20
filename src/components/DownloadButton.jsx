19f31c13a83e0de8dced9bf9c8837bbef4fa4afc
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-10-20 12:25:19 +0700

    fix: layout responsiveness and add anchor links

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,6 +1,6 @@
 export const DownloadButton = () => {
   return (
-    <a href="#" className="py-4 px-8 rounded-xl bg-white border-primary border-2 text-tertiary w-fit font-semibold">
+    <a href="https://drive.google.com/file/d/1PCYMxq4G1EjwmiZ8HqkYwgFwbl1aITSz/view?usp=sharing" className="py-4 px-8 rounded-xl bg-white border-primary border-2 text-tertiary w-fit font-semibold">
       Guidebook
     </a>
   )
