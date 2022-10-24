af0ba744e3fa0ca158608412a35b60a68b8357ad
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-10-24 09:00:37 +0700

    fix: guideline book link

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,6 +1,6 @@
 export const DownloadButton = () => {
   return (
-    <a href="https://drive.google.com/file/d/1PCYMxq4G1EjwmiZ8HqkYwgFwbl1aITSz/view?usp=sharing" className="py-4 px-8 rounded-xl bg-secondary text-tertiary w-fit font-semibold">
+    <a href="https://s.id/guidebookhoki2" className="py-4 px-8 rounded-xl bg-secondary text-tertiary w-fit font-semibold">
       Guidebook
     </a>
   )
