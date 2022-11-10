26c86bdcf089d75109b21e1a28acb6a0d893cd7f
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2022-11-10 13:04:11 +0700

    feat: add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import image from '@astrojs/image';
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
-});
+  integrations: [
+    tailwind(),
+    image(),
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
+})
